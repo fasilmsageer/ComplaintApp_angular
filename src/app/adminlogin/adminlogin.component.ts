d6078a44e96a081686694ab1ffde0af7cb26e1b1
fasilmsageer <fasilmsageer@gmail.com>
2022-12-11 19:41:34 +0530

    navbar

--- src/app/adminlogin/adminlogin.component.ts
+++ src/app/adminlogin/adminlogin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-adminlogin',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./adminlogin.component.css']
 })
 export class AdminloginComponent {
+  constructor(private route:Router){}
+
+username=""
+password=""
+
+readvalue = ()=>{
+ if(this.username=="admin"&&this.password=="12345"){
+  this.route.navigate(["/dashboard"])
+
+
+ }
+}
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from './app.component';
 import { AdminloginComponent } from './adminlogin/adminlogin.component';
 import { UserloginComponent } from './userlogin/userlogin.component';
 import { UserregisterComponent } from './userregister/userregister.component';
+import { UserprofileComponent } from './userprofile/userprofile.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminloginComponent,
     UserloginComponent,
-    UserregisterComponent
+    UserregisterComponent,
+    UserprofileComponent,
+    DashboardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.for(myRoute),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
